588cab7e20ec0b03235315537915ceba43e8bf2e
maicongavino <maiconserrao57@gmail.com>
2022-12-29 13:29:33 -0400

    progress 01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,23 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-def main():
-    df = pd.read_excel("inpi_db.xlsx", decimal='.')
-    st.dataframe(df)
+df = pd.read_excel("data.xlsx", decimal='.')
+
+def gera_grafico(graf):
+    st.dataframe([df[graf],df['Ano']])
+    fig = px.bar(df, x=df["Ano"], y=df[graf],height=400)
+    st.plotly_chart(fig, use_container_width = False)
     
+
+def main(): 
+    st.dataframe(df)
+    page = st.sidebar.selectbox('Selecione o Estado', [x for x in df.columns if x in ["Acre","Alagoas","Amazonas","Amapá","Bahia","Ceará",
+                                                                                      "Distrito Federal","Espírito Santo","Goiás","Maranhão", "Minas Gerais",
+                                                                                      "Mato Grosso do Sul","Mato Grosso","Pará","Paraíba","Pernambuco",
+                                                                                      "Piauí","Paraná","Rio de Janeiro","Rio Grande do Norte",
+                                                                                      "Rondônia", "Roraima", "Rio Grande do Sul","Santa Catarina","Sergipe",
+                                                                                      "São Paulo", "Tocantins"]])
+    gera_grafico(page)
 if __name__ == '__main__':
     main()
